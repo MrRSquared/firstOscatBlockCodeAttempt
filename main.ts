55ae84024ac555bd16cc55a92b3609eeb97025c9
Mr. Roth-Ritchie <jrothritchie@hdsd.org>
2021-01-24 23:12:03 -0500

    Updated GUI

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+Oscats.robotPeriodic(function () {
+    radio.sendValue("Battery", Rover.BatteryLevel())
+})
+Oscats.robotInit(function () {
+    radio.setGroup(1)
+})
+Oscats.teleopPeriodic(function () {
+    Rover.MotorRunDual(joystickbit.getRockerValue(joystickbit.rockerType.X) - joystickbit.getRockerValue(joystickbit.rockerType.Y), joystickbit.getRockerValue(joystickbit.rockerType.X) - joystickbit.getRockerValue(joystickbit.rockerType.Y))
+})
+Oscats.autonomousPeriodic(function () {
+    while (Rover.Ultrasonic() < 10) {
+        Rover.MotorRunDual(50, 50)
+        Rover.MotorStopAll(MotorActions.Stop)
+    }
+})
