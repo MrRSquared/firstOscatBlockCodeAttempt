cc7e90f734a7c3f334f7c7dc346306a99803ea11
Mr. Roth-Ritchie <jrothritchie@hdsd.org>
2021-01-24 23:13:11 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ Oscats.robotInit(function () {
     radio.setGroup(1)
 })
 Oscats.teleopPeriodic(function () {
-    Rover.MotorRunDual(joystickbit.getRockerValue(joystickbit.rockerType.X) - joystickbit.getRockerValue(joystickbit.rockerType.Y), joystickbit.getRockerValue(joystickbit.rockerType.X) - joystickbit.getRockerValue(joystickbit.rockerType.Y))
+    Rover.MotorRunDual(joystickbit.getRockerValue(joystickbit.rockerType.X) - joystickbit.getRockerValue(joystickbit.rockerType.Y), joystickbit.getRockerValue(joystickbit.rockerType.X) + joystickbit.getRockerValue(joystickbit.rockerType.Y))
 })
 Oscats.autonomousPeriodic(function () {
     while (Rover.Ultrasonic() < 10) {
