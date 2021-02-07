121c05b01f233a92c97b5bf4f6afd56c703438e5
Mr. Roth-Ritchie <jrothritchie@hdsd.org>
2021-02-07 13:28:05 -0500

    Simplified initial view

--- main.ts
+++ main.ts
@@ -1,28 +1,6 @@
-let strip: neopixel.Strip = null
-Oscats.robotPeriodic(function () {
-    basic.showString("" + (Oscats.getTimer()))
-})
-Oscats.robotInit(function () {
-    strip = neopixel.create(DigitalPin.P15, 2, NeoPixelMode.RGB)
-})
 Oscats.teleopPeriodic(function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    strip.show()
-})
-Oscats.disabledPeriodic(function () {
-    DFRobotMaqueenPlus.mototStop(Motors.ALL)
-    strip.clear()
-    strip.show()
-    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBL, Color.RED)
+    basic.showIcon(IconNames.Heart)
 })
 Oscats.autonomousPeriodic(function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-    strip.show()
-    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 66)
-})
-basic.forever(function () {
-    Rover.MotorRunDual(50, 50)
-})
-basic.forever(function () {
-    Rover.MotorRunDual(50, 50)
+    basic.showIcon(IconNames.Happy)
 })
