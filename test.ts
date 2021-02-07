86ce1846e4628ef07ad6c29c0ba710f07d90f8f7
Mr. Roth-Ritchie <jrothritchie@hdsd.org>
2021-02-07 13:30:54 -0500

    Update main.blocks, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+Oscats.teleopPeriodic(function () {
+    basic.showIcon(IconNames.Heart)
+})
+Oscats.autonomousPeriodic(function () {
+    basic.showIcon(IconNames.Happy)
+})
